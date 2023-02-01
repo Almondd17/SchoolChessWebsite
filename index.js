c8ff5c1ef93e2fcc8611fd74289d26e237f46feb
Shaked <116758564+Almondd17@users.noreply.github.com>
2023-02-01 13:10:38 +0000

    contact-function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function showBar(){
   document.getElementById('sidebar').classList.toggle('active')
 }
+
+function showInf(){
+  document.getElementById("contact-info").style.display = "block";
+  
+}
