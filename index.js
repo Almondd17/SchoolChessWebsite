a068f3b4202a0469c58b0fb690172aa504a40169
Almondd17 <116758564+Almondd17@users.noreply.github.com>
2023-03-14 13:05:49 +0200

    failed attempt for working searchbox using javascript

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ function showBar(){
 function showInf(){
   document.getElementById('contact-info').style.display = "block";
 }
+
+let options = [
+  'play chess 1v1',
+  'website Information',
+  'game rules',
+  'contact us',
+  'settings',
+  'register',
+  'online chess',
+];
+
+const resultsBox = document.querySelector(".results-box");
+const inputBox = document.getElementById("search-item");
+
+inputBox.onkeyup = function(){
+  let result = [];
+  let input = inputBox.value;
+  if(input.length){
+    result = options.filter((keyword)=>{
+    return keyword.toLowerCase().includes(input.toLowerCase()); 
+    });
+  }
+  display(result);
+}
+
+function display(result){
+  const content = result.map((list)=>{
+    return "<li>"+list+"</li>";
+  });
+  resultsBox.innerHTML = "<ul>"+ content.join('') +"</ul>"
+}
