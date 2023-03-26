bbd7fcdf796a3b20477092afe3ee5a01a5251256
Shaked <116758564+Almondd17@users.noreply.github.com>
2023-03-26 05:44:57 +0000

    failed attempt for search input display results

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,24 @@
 function showBar(){
-  document.getElementById('sidebar').classList.toggle('active')
+  document.getElementById('sidebar').classList.toggle('active');
 }
+let SearchInput = document.getElementById("search-item");
+let ResultsBox = document.getElementById("results-box");
+var results = [
+  'result1',
+  'result2',
+  'result3'
+];
+
+results.forEach(addResults);
+
+function addResults(){
+  const ResultList = ResultsBox.appendChild.document.createElement("ul");
+  ResultList.appendChild.document.createElement("li");
+  const listItem = document.getElementsByTagName("li") = results[indexedDB];
+}
+
+function showResults(){
+  SearchInput.onkeyup(listItem.style.display = 'block')
+}
+   
+ 
