9b48dfff8245d417f9e1be426481df1af78a7384
Almondd17 <116758564+Almondd17@users.noreply.github.com>
2023-04-29 13:37:58 +0300

    update status function on chessboard

--- javascript/chessPage.js
+++ javascript/chessPage.js
@@ -4,7 +4,24 @@ var options = {
     animated: false,
     width: 500,
     markLastMove: false,
-    animationSpeed: 5
+    animationSpeed: 5,
+
 };
+//create the board
 abChess = new AbChess("chessboard", options);
+var movesCount = 0;
+var notify = document.getElementById("notification");
+function updateboard(){
+    var status = " to move.";
+    var turn = "White";
+    movesCount += 1;
+    if (abChess.getActiveColor(movesCount) === "b") {
+        turn = "Black"
+    }
+    if (abChess.isCheck(movesCount)) {
+        status = " is in check.";
+    }
+    notify.innerText = turn + status;
+}
 abChess.setFEN();
+abChess.onMovePlayed(updateboard);
