3c33c076daff905d0ed2e8f9f74d392224ee2c72
Almondd17 <116758564+Almondd17@users.noreply.github.com>
2023-05-02 19:41:42 +0300

    sql jdbc connector attempt

---
+++ jdbc/connector.java
@@ -0,0 +1,21 @@
+package jdbc;
+import java.sql.*;
+public class connector{
+    static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    static final String USER = "root";
+    static final String Pass = "157521";
+
+    public static void main(String[] args) {
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, Pass);
+        Statement stmt = conn.createStatement();) {
+            String sql = "SELECT * FROM users";
+            stmt.executeUpdate(sql);
+            System.out.println("data base shown!");
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
